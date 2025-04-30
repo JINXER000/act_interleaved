d65f058c5bd4643ff72a01bf30873e6bbea86e9e
JoannaHXU <hang017@e.ntu.edu.sg>
2025-04-30 16:48:37 +0800

    modify Gripper_open/close value

--- aloha_scripts/constants.py
+++ aloha_scripts/constants.py
@@ -58,7 +58,18 @@ TASK_CONFIGS = {
         'episode_len': 600,
         'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
     },
-}
+    'clean_cup':{
+        'dataset_dir': DATA_DIR + '/clean_cup',
+        'num_episodes': 20,
+        'episode_len': 500,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'handoff_cup':{
+        'dataset_dir': DATA_DIR + '/handoff_cup',
+        'num_episodes': 30,
+        'episode_len': 600,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },}
 
 # DATA_DIR = '/home/xuhang/Desktop/aloha_data'
 # TASK_CONFIGS = {
@@ -91,7 +102,7 @@ TASK_CONFIGS = {
 ### ALOHA fixed constants
 DT = 0.02
 JOINT_NAMES = ["waist", "shoulder", "elbow", "forearm_roll", "wrist_angle", "wrist_rotate"]
-START_ARM_POSE = [0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239,  0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239]
+START_ARM_POSE = [0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239,  0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.005] #-0.02239
 
 # Left finger position limits (qpos[7]), right_finger = -1 * left_finger
 MASTER_GRIPPER_POSITION_OPEN = 0.02417
@@ -100,10 +111,10 @@ PUPPET_GRIPPER_POSITION_OPEN = 0.05800
 PUPPET_GRIPPER_POSITION_CLOSE = 0.01844
 
 # Gripper joint limits (qpos[6])
-MASTER_GRIPPER_JOINT_OPEN = 0.3083
-MASTER_GRIPPER_JOINT_CLOSE = -0.6842
-PUPPET_GRIPPER_JOINT_OPEN = 1.4910
-PUPPET_GRIPPER_JOINT_CLOSE = -0.6213
+MASTER_GRIPPER_JOINT_OPEN = 0.84982 #0.3083
+MASTER_GRIPPER_JOINT_CLOSE = -0.024543 #-0.6842
+PUPPET_GRIPPER_JOINT_OPEN = 0.84675 #1.4910
+PUPPET_GRIPPER_JOINT_CLOSE = -0.46786 #-0.6213
 
 ############################ Helper functions ############################
 
