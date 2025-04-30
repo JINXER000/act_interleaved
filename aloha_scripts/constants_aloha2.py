aba62499ba8020708a2a61cc6c7b14ad71b06ca0
JoannaHXU <hang017@e.ntu.edu.sg>
2025-04-30 17:17:34 +0800

    add aloha2 constants

---
+++ aloha_scripts/constants_aloha2.py
@@ -0,0 +1,141 @@
+### Task parameters
+
+
+DATA_DIR = '/ssd1/aloha_data'
+TASK_CONFIGS = {
+    'aloha_starbucks':{
+        'dataset_dir': DATA_DIR + '/aloha_starbucks',
+        'num_episodes': 50,
+        'episode_len': 450,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'aloha_conveyor':{
+        'dataset_dir': DATA_DIR + '/aloha_conveyor',
+        'num_episodes': 50,
+        'episode_len': 600,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'aloha_transfer_tape':{
+        'dataset_dir': DATA_DIR + '/aloha_transfer_tape',
+        'num_episodes': 30,
+        'episode_len': 500,
+        # 'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist', 'high_depth']
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'cup_random':{
+        'dataset_dir': DATA_DIR + '/cup_random',
+        'num_episodes': 20,
+        'episode_len': 500,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'screwdriver':{
+        'dataset_dir': DATA_DIR + '/screwdriver',
+        'num_episodes': 50,
+        'episode_len': 800,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'screwdriver_random':{
+        'dataset_dir': DATA_DIR + '/screwdriver_random',
+        'num_episodes': 50,
+        'episode_len': 750,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+        'fold':{
+        'dataset_dir': DATA_DIR + '/fold',
+        'num_episodes': 50,
+        'episode_len': 1000,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'hang_pants':{
+        'dataset_dir': DATA_DIR + '/hang_pants',
+        'num_episodes': 50,
+        'episode_len': 1000,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'harrypotter':{
+        'dataset_dir': DATA_DIR + '/harrypotter',
+        'num_episodes': 50,
+        'episode_len': 600,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'clean_cup':{
+        'dataset_dir': DATA_DIR + '/clean_cup',
+        'num_episodes': 20,
+        'episode_len': 500,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'handoff_cup':{
+        'dataset_dir': DATA_DIR + '/handoff_cup',
+        'num_episodes': 30,
+        'episode_len': 600,
+        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    },}
+
+# DATA_DIR = '/home/xuhang/Desktop/aloha_data'
+# TASK_CONFIGS = {
+#     'aloha_screwdriver':{
+#         'dataset_dir': DATA_DIR + '/aloha_screwdriver',
+#         'num_episodes': 50,
+#         'episode_len': 800,
+#         'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+#     },
+# }
+
+# DATA_DIR = '/home/xuhang/Desktop/aloha_data'
+# TASK_CONFIGS = {
+#     'aloha_board_ram':{
+#         'dataset_dir': DATA_DIR + '/aloha_board_ram',
+#         'num_episodes': 50,
+#         'episode_len': 750,
+#         'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+#     },
+# }
+# TASK_CONFIGS = {
+#     'aloha_ziploc':{
+#         'dataset_dir': DATA_DIR + '/aloha_ziploc_new',
+#         'num_episodes': 50,
+#         'episode_len': 500,
+#         'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+#     },
+# }
+
+### ALOHA fixed constants
+DT = 0.02
+JOINT_NAMES = ["waist", "shoulder", "elbow", "forearm_roll", "wrist_angle", "wrist_rotate"]
+START_ARM_POSE = [0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239,  0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.005] #-0.02239
+
+# Left finger position limits (qpos[7]), right_finger = -1 * left_finger
+MASTER_GRIPPER_POSITION_OPEN = 0.02417
+MASTER_GRIPPER_POSITION_CLOSE = 0.01244
+PUPPET_GRIPPER_POSITION_OPEN = 0.05800
+PUPPET_GRIPPER_POSITION_CLOSE = 0.01844
+
+# Gripper joint limits (qpos[6])
+MASTER_GRIPPER_JOINT_OPEN = 0.84982 #0.3083
+MASTER_GRIPPER_JOINT_CLOSE = -0.024543 #-0.6842
+PUPPET_GRIPPER_JOINT_OPEN = 0.84675 #1.4910
+PUPPET_GRIPPER_JOINT_CLOSE = -0.46786 #-0.6213
+
+############################ Helper functions ############################
+
+MASTER_GRIPPER_POSITION_NORMALIZE_FN = lambda x: (x - MASTER_GRIPPER_POSITION_CLOSE) / (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE)
+PUPPET_GRIPPER_POSITION_NORMALIZE_FN = lambda x: (x - PUPPET_GRIPPER_POSITION_CLOSE) / (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE)
+MASTER_GRIPPER_POSITION_UNNORMALIZE_FN = lambda x: x * (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE) + MASTER_GRIPPER_POSITION_CLOSE
+PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN = lambda x: x * (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE) + PUPPET_GRIPPER_POSITION_CLOSE
+MASTER2PUPPET_POSITION_FN = lambda x: PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN(MASTER_GRIPPER_POSITION_NORMALIZE_FN(x))
+
+MASTER_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - MASTER_GRIPPER_JOINT_CLOSE) / (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE)
+PUPPET_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - PUPPET_GRIPPER_JOINT_CLOSE) / (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE)
+MASTER_GRIPPER_JOINT_UNNORMALIZE_FN = lambda x: x * (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE) + MASTER_GRIPPER_JOINT_CLOSE
+PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN = lambda x: x * (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE) + PUPPET_GRIPPER_JOINT_CLOSE
+MASTER2PUPPET_JOINT_FN = lambda x: PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN(MASTER_GRIPPER_JOINT_NORMALIZE_FN(x))
+
+MASTER_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE)
+PUPPET_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE)
+
+MASTER_POS2JOINT = lambda x: MASTER_GRIPPER_POSITION_NORMALIZE_FN(x) * (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE) + MASTER_GRIPPER_JOINT_CLOSE
+MASTER_JOINT2POS = lambda x: MASTER_GRIPPER_POSITION_UNNORMALIZE_FN((x - MASTER_GRIPPER_JOINT_CLOSE) / (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE))
+PUPPET_POS2JOINT = lambda x: PUPPET_GRIPPER_POSITION_NORMALIZE_FN(x) * (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE) + PUPPET_GRIPPER_JOINT_CLOSE
+PUPPET_JOINT2POS = lambda x: PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN((x - PUPPET_GRIPPER_JOINT_CLOSE) / (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE))
+
+MASTER_GRIPPER_JOINT_MID = (MASTER_GRIPPER_JOINT_OPEN + MASTER_GRIPPER_JOINT_CLOSE)/2
